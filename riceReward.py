7610b6181543412cfc34f101eb14a2b6a267845a
Diomedes Git <diomedesgit@gmail.com>
2023-03-24 20:57:27 +0000

    adding support for even larger numbers to accommodate a 100 sqare board

--- riceReward.py
+++ riceReward.py
@@ -1,4 +1,4 @@
-squares = input('okay, so how many squares are there on a chessboard? ')
+squares = input("okay, so there are 64 square on a chess board, and 100 on a snakes and ladders board, so how many squares are there on the board you have?  ")
 
 reward = f'{(1<<(int(squares))):,}'
 print(reward)
@@ -14,5 +14,13 @@ if reward.count(",") == 4:
 if reward.count(",") == 5:
     print ("bro, that's quadrillions of grains!!!!!!")
 if reward.count(",") == 6:
-    print ("bro, that's quintillions of grains!!!!!!")
+    print ("bro, that's quintillions of grains!!!!!!!")
+if reward.count(",") == 7:
+    print ("bro, that's sextillions of grains!!!!!!!!")
+if reward.count(",") == 8:
+    print ("bro, that's septtillions of grains!!!!!!!!!")  
+if reward.count(",") == 9:
+    print ("bro, that's octtillions of grains!!!!!!!!!!")   
+if reward.count(",") == 10:
+    print ("bro, that's nonillions of grains!!!!!!!!!!!")    
 
