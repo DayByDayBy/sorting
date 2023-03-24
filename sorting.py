e598dc7052666dfde391f0532f75c3ad0e364de7
Diomedes Git <diomedesgit@gmail.com>
2023-03-24 20:19:15 +0000

    first commit

---
+++ sorting.py
@@ -0,0 +1,9 @@
+list = [1, 2, 3, 4, 5, 6, 7]
+backwardList = list[::-1]
+print(list)
+print(backwardList)
+
+newList = [3, 56, 7, 21, 1, 9, 303, 9001]
+sortedList = sorted(newList)
+print(list)
+print(sortedList)
