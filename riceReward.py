15005b84f099465bf96492bb806b0b8f34d325f3
Diomedes Git <diomedesgit@gmail.com>
2023-03-24 20:59:19 +0000

    no way, etc

--- riceReward.py
+++ riceReward.py
@@ -4,23 +4,26 @@ reward = f'{(1<<(int(squares))):,}'
 print(reward)
 
 if reward.count(',') == 1:
-    print ("bro, that's thousands of grains!")
+    print ("no way bro, that's thousands of grains!")
 if reward.count(",") == 2:
-    print ("bro, that's millions of grains!!")
+    print ("no way bro, that's millions of grains!!")
 if reward.count(",") == 3:
-    print ("bro, that's billions of grains!!!")
+    print ("no way bro, that's billions of grains!!!")
 if reward.count(",") == 4:
-    print ("bro, that's trillions of grains!!!!")
+    print ("no way bro, that's trillions of grains!!!!")
 if reward.count(",") == 5:
-    print ("bro, that's quadrillions of grains!!!!!!")
+    print ("no way bro, that's quadrillions of grains!!!!!!")
 if reward.count(",") == 6:
-    print ("bro, that's quintillions of grains!!!!!!!")
+    print ("no way bro, that's quintillions of grains!!!!!!!")
 if reward.count(",") == 7:
-    print ("bro, that's sextillions of grains!!!!!!!!")
+    print ("no way bro, that's sextillions of grains!!!!!!!!")
 if reward.count(",") == 8:
-    print ("bro, that's septtillions of grains!!!!!!!!!")  
+    print ("no way bro, that's septtillions of grains!!!!!!!!!")  
 if reward.count(",") == 9:
-    print ("bro, that's octtillions of grains!!!!!!!!!!")   
+    print ("no way bro, that's octtillions of grains!!!!!!!!!!")   
 if reward.count(",") == 10:
-    print ("bro, that's nonillions of grains!!!!!!!!!!!")    
+    print ("no way bro, that's nonillions of grains!!!!!!!!!!!")   
+
+
+ 
 
